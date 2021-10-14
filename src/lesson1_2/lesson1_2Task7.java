5fb6e94008fe93aeb2ce38356707f7ed47f6ce91
ValeraVialeska <valera.vialeska@gmail.com>
2021-10-14 04:39:30 +0300

    Completed 6 tasks Lesson1_2 homework

--- src/lesson1_2/lesson1_2Task7.java
+++ src/lesson1_2/lesson1_2Task7.java
@@ -1,4 +1,4 @@
-package lesson1_2;
+ package lesson1_2;
 /**7) (Дополнительно) В переменную записываете количество программистов. В
  зависимости от количества программистов необходимо вывести правильно
  окончание. Например:
@@ -12,6 +12,30 @@ package lesson1_2;
 
  */
 public class lesson1_2Task7 {
+    public static void main(String[] args) {
 
+        int programmer = 56;
+
+        if (programmer > 100 );{
+            System.out.println(programmer %= 100);
+        }
+             if (programmer < 100);{
+            System.out.println(programmer %= 10);
+        }
+        
+        if (programmer == 1);{
+            System.out.println("программист");
+        }
+
+        if (programmer == 11 || programmer == 12 || programmer == 13 || programmer == 14);{
+            System.out.println("программистов");
+        }
+        if (programmer == 2 || programmer == 3 || programmer == 4);{
+            System.out.println("программиста");
+        }if (programmer == 5 || programmer == 6 || programmer == 7 || programmer == 8 || programmer == 9);{
+            System.out.println("программистов");
+        }
+
+    }
 }
-
+/** что то я не понимаю почему оно не считывает нужное значение  а просто все*/
