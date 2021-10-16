01b536233198fd7321920403486e1f3420b90895
ValeraVialeska <valera.vialeska@gmail.com>
2021-10-17 00:32:47 +0300

    Completed tasks Lesson1_2 homework

--- src/lesson1_2/lesson1_2Task7.java
+++ src/lesson1_2/lesson1_2Task7.java
@@ -14,20 +14,29 @@
 public class lesson1_2Task7 {
     public static void main(String[] args) {
 
-        int programmer = 99;
+       int programmer = 22523;
 
         if (programmer % 10 == 1 || programmer % 100 == 1){
             System.out.println(programmer + "программист");
         }
-        if (programmer % 10 == 2 || programmer % 100 == 2 || programmer % 10 == 3 || programmer % 100 == 3 || programmer % 10 == 4 || programmer % 100 == 4){
+        else if (programmer % 10 == 2 || programmer % 100 == 2 ||
+            programmer % 10 == 3 || programmer % 100 == 3 ||
+            programmer % 10 == 4 || programmer % 100 == 4){
             System.out.println(programmer +"программиста");
         }
-        if (programmer % 10 == 11 || programmer % 100 == 11 || programmer % 10 == 12 || programmer % 100 == 12 || programmer % 10 == 13 || programmer % 100 == 13 || programmer % 10 == 14 || programmer % 100 == 14){
+        else if (programmer % 10 == 11 || programmer % 100 == 11 ||
+            programmer % 10 == 12 || programmer % 100 == 12 ||
+            programmer % 10 == 13 || programmer % 100 == 13 ||
+            programmer % 10 == 14 || programmer % 100 == 14){
             System.out.println(programmer +"программистов");
         }
-        else if (programmer % 10 == 5 || programmer % 100 == 5 || programmer % 10 == 6 || programmer % 100 == 6 || programmer % 10 == 7 || programmer % 100 == 7 || programmer % 10 == 8 || programmer % 100 == 8 || programmer % 10 == 9 || programmer % 100 == 9) {
+        else {
             System.out.println(programmer + "программистов");
         }
-        }
-}
-/** В общем и получилось,и как бы нет я считаю.Хотелось бы услышать ваши коментарии по поводу укорочения кода,и я так понимаю если числа больше ста то работать ничего не будет. */
+    }
+    }
+
+
+
+
+
